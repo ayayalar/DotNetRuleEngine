bd073f92550aec50eb28485c1d534daf82181558
Arif Yayalar <ayayalar@mac.com>
2019-12-31 15:34:40 -0800

    Removed unused method GetServices

--- DotNetRuleEngine.Interface/IDependencyResolver.cs
+++ DotNetRuleEngine.Interface/IDependencyResolver.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace DotNetRuleEngine.Interface
 {
     public interface IDependencyResolver
     {
         object GetService(Type serviceType);
-
-        IEnumerable<object> GetServices(Type serviceType);
     }
 }
